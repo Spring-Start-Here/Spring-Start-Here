bf9e7de71b1761245f21dd05a596f17833cc3bb5
Laurentiu Spilca <Laurentiu.Spilca@endava.com>
2026-03-22 11:48:18 +0200

    CH7

---
+++ ssh_ch7_ex1/src/main/java/com/example/ssh_ch7_ex1/controllers/MainController.java
@@ -0,0 +1,13 @@
+package com.example.ssh_ch7_ex1.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "home.html";
+    }
+}
